b4b6c1a2ca294ce13a2aabd7a8c7fd4b79f49f47
FranRGit <francisrgfr@gmail.com>
2023-10-22 00:41:11 -0500

    Estructura inicial de clases

--- src/Sistema/Clases/Modulo.java
+++ src/Sistema/Clases/Modulo.java
@@ -1,15 +1,13 @@
-package Sistema.Clases;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package Sistema.Clases;
 
 /**
  *
  * @author USUARIO
  */
-public class Alumno {
-    private String codigoUsuarioAlumno;
+public class Modulo {
     
 }
