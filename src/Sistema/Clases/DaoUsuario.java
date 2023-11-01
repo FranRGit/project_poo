4d9404f0c57665eed2d2793c6691684557cf03c2
FranRGit <francisrgfr@gmail.com>
2023-11-01 16:30:01 -0500

    Usuario db

---
+++ src/Sistema/Clases/DaoUsuario.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Sistema.Clases;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Usuario
+ */
+public class DaoUsuario {
+
+
+
+    public DaoUsuario() {
+       
+    }
+
+    public void agregarUsuario(Usuario usuario) {
+        
+    }
+
+    public void eliminarUsuario(Usuario usuario) {
+       
+    }
+
+    public Usuario buscarUsuarioPorCodigo(String codigo) {
+       
+        return null;
+       
+    }
+
+    public void mostrarUsuarios() {
+        
+    }
+
+    public int validarCodigoUsuario(String codigo) {
+       
+        return 0;
+       
+    }
+
+    public Usuario getUsuario(int posicion) {
+        
+        return null;
+        
+    }
+}
